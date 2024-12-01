39d3bfb8c655ea91b242f6856977b8876e2aeaf6
Marta Lionaite <martalionaite@gmail.com>
2024-12-01 21:35:56 +0200

    pataisymas

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import './styles/main.scss';
 import { setupRandomMealButton } from './modules/randomMeal.js';
 import { setupSearch } from './modules/search';
 import { populateFilters, setupFilters } from './modules/filter';
-import { setupImages } from './modules/images.js'
+import { setupHeroImage, setupImages } from './modules/images.js'
 
 document.addEventListener('DOMContentLoaded', async () => {
   setupImages();
+  setupHeroImage(); // Nustato „Hero“ sekcijos paveikslėlį
   setupRandomMealButton();
   setupSearch();
   await populateFilters();
--- src/modules/images.js
+++ src/modules/images.js
@@ -1,5 +1,6 @@
 import salad from '../images/salad.jpg';
 import soup from '../images/soup.jpg';
+import plateFood from '../images/plate-food.jpg';
 
 export function setupImages() {
 const saladCard = document.querySelector('.recipe-card:nth-child(1)');
@@ -25,5 +26,16 @@ if (saladCard) {
     }
   } else {
     console.error('Elementas su klase "recipe-card:nth-child(2)" nerastas.');
+  }
+}
+
+/* galimai trinti */
+export function setupHeroImage() {
+  const heroImg = document.getElementById('hero-img');
+  if (heroImg) {
+    heroImg.src = plateFood; // Nustatome paveikslėlio kelią
+    heroImg.alt = 'Delicious Meal'; // Pridedame alternatyvų tekstą
+  } else {
+    console.error('Hero image elementas nerastas.');
   }
 }
